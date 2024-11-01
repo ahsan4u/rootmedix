15188e15217016a37bd9758fc94eafcc9f7473a4
Mohd Ahsan <mohdahsanahsan1@gmail.com>
2024-11-01 17:36:21 +0530

    first commit

---
+++ app/about/page.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+export default function AboutPage() {
+    return (
+      <>
+      <Link href="/">
+        <h1 className="text-5xl font-bold text-center">About Page Click to go Home</h1>
+      </Link>
+      </>
+    )
+  }
